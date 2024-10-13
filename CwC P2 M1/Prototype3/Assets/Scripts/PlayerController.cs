93ecf6e8eababd97b8f6e6b636a06b3c738acbf2
BenjaminMillis <bmillis01@qub.ac.uk>
2024-10-13 18:30:53 +0100

    Completed 3.3

--- CwC P2 M1/Prototype3/Assets/Scripts/PlayerController.cs
+++ CwC P2 M1/Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,24 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier = 1;
     public bool isOnGround = true;
-    public bool gameOver;
+    public bool gameOver = false;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround){
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -32,6 +35,8 @@ public class PlayerController : MonoBehaviour
         else if(collision.gameObject.CompareTag("Obstacle")){
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
 
     }
