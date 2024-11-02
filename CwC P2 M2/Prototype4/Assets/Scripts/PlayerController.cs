e20e631cc3aecc839f211cee730ba46abbf5049e
BenjaminMillis <bmillis01@qub.ac.uk>
2024-11-02 19:36:09 +0000

    Completed Create with Code 4.1

---
+++ CwC P2 M2/Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+
+    void Start(){
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    void Update(){
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
