21d416b69fb42c00551092d0bca56f89f961eb71
BenjaminMillis <bmillis01@qub.ac.uk>
2024-11-11 00:21:57 +0000

    Completed Create with Code 5.3

--- CwC P2 M3/Prototype5/Assets/Scripts/GameManager.cs
+++ CwC P2 M3/Prototype5/Assets/Scripts/GameManager.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
 
     private float spawnRate = 1.0f;
     private int score;
 
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
     }
 
+    public void GameOver(){
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
     IEnumerator SpawnTarget(){
-        while(true){
+        while(isGameActive){
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
@@ -30,5 +42,9 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd){
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
