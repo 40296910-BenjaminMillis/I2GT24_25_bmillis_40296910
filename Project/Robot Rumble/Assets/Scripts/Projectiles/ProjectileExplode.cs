9acfa4be4b71b7f4912f7763c92b3c652709ff02
BenjaminMillis <bmillis01@qub.ac.uk>
2024-11-24 14:48:57 +0000

    Added explosions to enemy rockets

--- Project/Robot Rumble/Assets/Scripts/Projectiles/ProjectileExplode.cs
+++ Project/Robot Rumble/Assets/Scripts/Projectiles/ProjectileExplode.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class ProjectileExplode : Projectile
 {
-    [SerializeField] float explosionRadius = 5f;
-    [SerializeField] float explosionForce = 300f;
-    [SerializeField] float upwardsModifier = 2.0f;
-    [SerializeField] ParticleSystem explosion;
+    [SerializeField] GameObject explosion;
     MoveType moveType;
 
     void Start(){
@@ -19,23 +16,9 @@ public class ProjectileExplode : Projectile
     }
 
     protected override void RemoveProjectile(){
-        Explode();
+        GameObject instance = Instantiate(explosion, transform.position, Quaternion.identity);
         base.RemoveProjectile();
     }
 
-    void Explode(){
-        ParticleSystem instance = Instantiate(explosion, transform.position, Quaternion.identity);
-        Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
-        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
 
-        foreach(Collider hit in colliders){
-            if(hit.GetComponent<ImpactReceiver>()){
-                Debug.Log("explosion hit player");
-            }
-
-            else if(hit.GetComponent<Rigidbody>()){
-                hit.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius, upwardsModifier);
-            }
-        }
-    }
 }
