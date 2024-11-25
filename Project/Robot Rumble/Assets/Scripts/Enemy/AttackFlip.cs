6a5155eb39096907edeae2cf84893dfabcbd42a3
BenjaminMillis <bmillis01@qub.ac.uk>
2024-11-25 23:39:10 +0000

    Animating the flipper enemy properly
    Flipper enemy now animates when attacking, and not randomly.
    I avoided fixing this for a while, not a huge deal and is likely to change if I update the model.

--- Project/Robot Rumble/Assets/Scripts/Enemy/AttackFlip.cs
+++ Project/Robot Rumble/Assets/Scripts/Enemy/AttackFlip.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AttackFlip : AttackType
 {
     [SerializeField] float launchForce;
+    [SerializeField] Animator animator;
 
      public override void Attack(){
         base.Attack();
@@ -13,24 +14,14 @@ public class AttackFlip : AttackType
         }
     }
 
-
     void OnTriggerStay(Collider other){
         // Flip the sucker!
-        
-        // I will need to change this, as this conflicts with the ontrigger of enemybehaviour
-        // I will probably make new scripts for that for each enemy type
-
         if(attackCooldown <= 0){
             if(other.tag == "Player"){
+                animator.SetTrigger("FlipTrigger");
                 // The player needs their own script to handle the force applied to them
                 other.GetComponent<ImpactReceiver>().AddImpact((transform.forward*1.5f) + Vector3.up, launchForce);
             }
-            
-            // Disabled for now, it sort of works but has weird interactions with prone enemies
-            // if(other.tag == "Enemy"){
-            //     other.GetComponent<Rigidbody>().AddForce((transform.forward + (Vector3.up*2)) * launchForce, ForceMode.Impulse);
-            //     StartCoroutine(other.GetComponent<EnemyBehaviour>().MakeProne());
-            // }
             attackCooldown = attackDelay;
         }
     }
