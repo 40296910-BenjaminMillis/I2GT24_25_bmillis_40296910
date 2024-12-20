19378bf2887a2f316bb21102f5e5b57501e39ffd
BenjaminMillis <bmillis01@qub.ac.uk>
2024-12-20 20:12:23 +0000

    Tried fixing the platforms rising too far above the arena

--- Project/Robot Rumble/Assets/Scripts/World/ArenaMover.cs
+++ Project/Robot Rumble/Assets/Scripts/World/ArenaMover.cs
@@ -10,10 +10,15 @@ public class ArenaMover : MonoBehaviour
     // Manage the movement of an arena, depending if it has been told to rise or lower
     void Update()
     {
-        if(transform.position.y >= -0.5)
+        if(transform.position.y >= -0.5){
             rising = false;
-        if(transform.position.y <= -25)
+            transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
+        }
+
+        if(transform.position.y <= -25){
             lowering = false;
+            new Vector3(transform.position.x, -25, transform.position.z);
+        }
 
         if(rising)
             transform.Translate(new Vector3(0, 20, 0) * Time.deltaTime);
