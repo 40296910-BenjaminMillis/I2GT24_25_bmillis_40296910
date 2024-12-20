5ecaf8547c11f4e05b212df06cb1e06801c26d0c
BenjaminMillis <bmillis01@qub.ac.uk>
2024-12-20 02:03:31 +0000

    Updated credits

--- Project/Robot Rumble/Assets/Scripts/Enemy/ProneType.cs
+++ Project/Robot Rumble/Assets/Scripts/Enemy/ProneType.cs
@@ -6,12 +6,18 @@ public class ProneType : MonoBehaviour
 {
     [SerializeField] protected int damage = 1; // Damage dealt to self and target when colliding
     protected bool isProne;
+    protected AudioPlayer audioPlayer;
+
+    void Start(){
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+    }
 
     // When colliding with another enemy, both enemies get hurt
     void OnTriggerEnter(Collider collider) {
         if(collider.CompareTag("Enemy") && isProne){
             collider.gameObject.GetComponent<Health>().UpdateHealth(-damage, 2);
             GetComponent<Health>().UpdateHealth(-damage, 2);
+            audioPlayer.PlayDashHitClip(this.transform.position);
         }
     }
 
